ff08ffbf87c5e3248121114b4f54f2aebe1895f3
Prof. Miguel Angel Medina-Pérez, PhD <47543798+miguelmedinaperez@users.noreply.github.com>
2021-01-29 17:16:33 -0600

    Update ICentroidsCalculator.cs

--- ICentroidsCalculator.cs
+++ ICentroidsCalculator.cs
@@ -1,10 +1,20 @@
-﻿using System;
+/*
+ * Created by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ * Created: 11/16/2016
+ * Comments by: Miguel Angel Medina Pérez (miguelmedinaperez@gmail.com)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace PRFramework.Core.Common
 {
+    /// <summary>
+    ///     The class implementing this interface allows creating a centroid of specific type of objects.
+    /// </summary>
+    /// <typeparam name="T">The type of object created by the factory.</typeparam>
     public interface ICentroidsCalculator<T>
     {
         IList<T> Calculate(IEnumerable<T> objects, IFactory<T> factory);
